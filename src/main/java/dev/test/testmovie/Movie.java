f33e062b523bd1b1224a62798987e964db2e60c1
Duc Dang <shadowdragonvt@gmail.com>
2023-03-27 18:39:42 -0500

    Test src

--- src/main/java/dev/test/testmovie/Movie.java
+++ src/main/java/dev/test/testmovie/Movie.java
@@ -10,7 +10,7 @@ import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.core.mapping.DocumentReference;
 
-@Document(collection = "movies")
+@Document(collection = "")
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
